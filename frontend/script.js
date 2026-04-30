fdeba1cf2491c7ef4e5de60d022325ed6f0597a7
Jaynor Mendoza <jaynormendoza2023@gmail.com>
2026-04-30 15:40:30 +0800

    fixed

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
-const API_URL = "skinscan-production.up.railway.app/predict";
-const HISTORY_URL = "skinscan-production.up.railway.app/history";
+const API_URL = "https://skinscan-production.up.railway.app/predict";
+const HISTORY_URL = "https://skinscan-production.up.railway.app/history";
 
 const fileInput = document.getElementById("file-upload");
 const uploadBtn = document.getElementById("upload-btn");
